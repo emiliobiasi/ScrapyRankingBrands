1aafd8f6ede78ef5d1225ada4085e46863434912
Emílio Biasi <ejbiasi@hotmail.com>
2023-06-09 17:30:26 -0300

    finalizando crawler

--- bd.py
+++ bd.py
@@ -2,16 +2,15 @@ import json
 from pymongo import MongoClient
 
 
-with open('marcas.json', 'r') as file:
-    data = json.load(file)
+def bd():
+    with open('marcas.json', 'r') as file:
+        data = json.load(file)
 
+    client = MongoClient(
+        'mongodb+srv://<user>:<password>@cluster0.aharvcg.mongodb.net/?retryWrites=true&w=majority')
+    database = client['brands']
+    collection = database['marcas']
 
-client = MongoClient('mongodb+srv://giandutra:giandutra@cluster0.xdo5ska.mongodb.net/?retryWrites=true&w=majority')
-database = client['brands']  
-collection = database['marcas']  
+    collection.insert_many(data)
 
-
-collection.insert_many(data)
-
-
-client.close()
+    client.close()
--- myspider.py
+++ myspider.py
@@ -2,89 +2,85 @@ import json
 
 import scrapy
 
+from bd import bd
 
 
 def urls_brands():
-    name = 'blogspider'
     start_urls = ['https://www.rankingthebrands.com/The-Brands-and-their-Rankings.aspx?catFilter=0&nameFilter=W']
+
+    # realizei apenas a captura de uma letra por conta do tempo de execução que daria captando todas as marcas,
+    # porem ficaria desta forma:
+
+    # base_url = 'https://www.rankingthebrands.com/The-Brands-and-their-Rankings.aspx?catFilter=0&nameFilter='
+    # alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # urls = list()
+    # for c in alphabet:
+    #     urls.append(base_url + c)
+    # return urls
+
     return start_urls
 
 
-def write_results(brands, gbin, websites, countries):
-    sorted_brands = sorted(brands, key=lambda d: d['brand_name'])
-    i = 0
-    for brand in sorted_brands:
-        brand_name = brand['brand_name']
-        brand['gbin'] = gbin[i]
-        brand['website'] = websites[i]
-        brand['country'] = countries[i]
-        i+=1
-        
+def write_results(names, images, gbin, websites, countries):
+    brands = []
+    for i in range(len(names)):
+        brand = {
+            'brand_name': names[i],
+            'image': images[i],
+            'gbin': gbin[i],
+            'website': websites[i],
+            'country': countries[i]
+        }
+        brands.append(brand)
 
-    jsonstring = json.dumps(sorted_brands)
+    jsonstring = json.dumps(brands)
     with open('marcas.json', 'w') as output_file:
         output_file.write(jsonstring)
+    bd()
 
 
 class BrandsSpider(scrapy.Spider):
     name = 'brands'
     start_urls = urls_brands()
-    brands = list()
     details_link = []
-    names = []
+    names = list()
     gbin = list()
     websites = list()
     countries = list()
+    images = list()
 
     def parse(self, response):
-        for title in response.css('.rankingName'):
-            brand_to_write = title.css('::text').get()
-            self.brands.append({'brand_name': brand_to_write})
-            yield {'title': title.css('::text').get()}
-
         for link in response.css('a::attr(href)').getall():
             if link.startswith('Brand-detail.aspx?brandID='):
                 self.details_link.append('https://www.rankingthebrands.com/' + link)
 
-        
         for link in self.details_link:
             yield scrapy.Request(link, callback=self.parse_details)
 
-        print(self.brands)
         print(self.details_link)
-        print(self.countries)
 
     def parse_details(self, response):
+
+        for c in response.css('.brandContainer'):
+            img = c.css('img::attr(src)').get()
+            name = c.css('span#ctl00_mainContent_LBBrandName::text').get()
+            self.images.append('https://www.rankingthebrands.com/' + str(img))
+
+            if name is not None:
+                self.names.append(name)
+
         for c in response.css('div.brandInfoRow'):
             g = c.css('span#ctl00_mainContent_LBLGBIN::text').get()
             website = c.css('a[rel="nofollow"]::text').get()
             country = c.css('span#ctl00_mainContent_LBCountryOfOrigin::text').get()
-            if g != None:
+            if g is not None:
                 self.gbin.append(g)
-                print(g)
-            if country != None:
+
+            if country is not None:
                 self.countries.append(country)
-                print(country)
-            if website != None and website != 'http://www.GBIN.info':
+
+            if website is not None and website != 'http://www.GBIN.info':
                 self.websites.append(website)
-                print(website)
-            
-
-        print(self.gbin)
-        print(self.countries)
-        print(self.websites)
-            
-
-        #for c in response.css('.element .style'):
-            #name = c.css('#ctl00_mainContent_LBBrandName::text').get()
-            #if name != None:
-                #self.names.append(name)
-                #print(name)
- 
-        #print(self.countries)
-        #print(self.name)
-
-    
 
     def close(self, reason):
-        write_results(self.brands, self.gbin, self.websites, self.countries)
+        write_results(self.names, self.images, self.gbin, self.websites, self.countries)
